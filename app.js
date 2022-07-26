12b258802529fa1e72054c005fb7e6655ef721f3
Francisco Guerrero <juanfg.8640@gmail.com>
2022-07-25 23:42:47 -0300

    Interacción DOM y eventos agregada V1

--- app.js
+++ app.js
@@ -11,31 +11,18 @@ const catalogo = [
 
 let carrito = [];
 
-let selector; //selector de opciones del menu
-
 /////////////////////////////////////////////////
 //////////////DEFINICIÓN DE FUNCIONES///////////
 ///////////////////////////////////////////////
 
-////////////////Función - Añadir items al carrito//////////////
+////////////////Función - Actualizar carrito (añadir y remover items) //////////////
 
-//si el Item está presente en el carrito se le suma 1. Si no está presente se pushea sku(id) y cantidad 1
-function sumarItem(sku) {
+function actualizarCarrito(sku, cantidad) {
   //logica para saber si el item se encuentra en el carrito
   if (carrito.find((producto) => sku === producto.id)) {
-    carrito[carrito.findIndex((producto) => producto.id === sku)].cantidad += 1;
-  } else carrito.push({ id: sku, cantidad: 1 });
-}
-
-//////////////Función - Eliminación items del carrito//////////////////
-
-//Si el item está en el carrito, se le resta 1 en cantidad. Si no está se avisa al usuario que no tiene ese item.
-//items con cantidad 0 se eliminan del carrito
-
-function quitarItem(sku) {
-  if (carrito.find((producto) => sku === producto.id)) {
-    carrito[carrito.findIndex((producto) => producto.id === sku)].cantidad -= 1;
-  } else alert('No tiene ese item en el carrito');
+    carrito[carrito.findIndex((producto) => producto.id === sku)].cantidad =
+      cantidad;
+  } else carrito.push({ id: sku, cantidad: cantidad });
   //Elimina del carrito items con cantidad 0
   carrito = carrito.filter((producto) => producto.cantidad > 0);
 }
@@ -49,6 +36,7 @@ function borrarCarrito() {
 //////////////// Función - Buscar Precio/Nombre desde el Catalogo según id de producto deseado////////////////
 
 function buscarPrecio(sku) {
+  console.log(typeof sku);
   const precioSku =
     catalogo[catalogo.findIndex((producto) => producto.id === sku)].precio;
   return precioSku;
@@ -83,8 +71,8 @@ function totalCarrito() {
   return total;
 }
 
-//////////////// Función - Mostrar catalogo --> Armado de array con productos + Cantidad seleccionada para mostrar mediante un Join en Menu de opciones////////////////////////////////////////////
-//usar For Of al momento de ir a DOM?/////////
+//////////////// Función - Mostrar catalogo --> Armado de array con productos + Cantidad seleccionada para mostrar mediante un Join al finalizar la compra (SACAR EN PROXIMAS ENTREGAS)///////////////////////////////////////
+
 function mostrarCatalogo() {
   const mostrarCatalogo = catalogo.map(
     (item) =>
@@ -96,82 +84,20 @@ function mostrarCatalogo() {
 }
 
 ///////////////////////////////////////////////
-////////////LÓGICA INTERACCIÓN USUARIO////////
-/////////////////////////////////////////////
-
-do {
-  selector = parseInt(
-    prompt(`Bienvenido a la Tienda de Alfombras!
-Utilice el menu para agregar productos al carrito y finalizar su compra. Para remover un producto del carrito ingrese el código con un "-" adelante (ej:-1)
-
-Carrito Actual = $${totalCarrito()}
-
-Productos:
-${mostrarCatalogo().join('\n')}
-
-0. Salir (cancela compra)
-9. Checkout
-10. Limpiar Carrito
-  `)
-  );
-  //Lógica de selección del usuario con las opciones del Menu.
-  switch (selector) {
-    case 0:
-      //Cancelación de compra del usuario y sale del programa
-      alert('Ha cancelado su compra. Gracias. Vuelva Pronto');
-      break;
-    //agregado de productos al carito
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-      sumarItem(selector);
-      break;
-    //quita de productos del carito
-    case -1:
-    case -2:
-    case -3:
-    case -4:
-      quitarItem(Math.abs(selector));
-      break;
-    case 9:
-      // revisamos si hay items en el carrito y cerramos la compra. Si se encuentra vacio devolvemos un error al usuario y volvemos al menu principal
-      if (carrito.length > 0) {
-        //Finalización de compra efectiva. Se muestra recibo final con productos y monto total.
-        alert(`Gracias por su compra!
-Su total es de $${totalCarrito()}
-
-Productos:
-${mostrarCatalogo().join('\n')}`);
-      } else {
-        alert(
-          'Su carrito está vacio. Por favor agregue items para finalizar su compra o cancele la misma desde el menu.'
-        );
-        selector = 'error';
-      }
-      break;
-    //borrado total del carrito
-    case 10:
-      borrarCarrito();
-      break;
-    default:
-      //Mensaje de error para opciones de menu no validas
-      alert(
-        'Opción no valida. Por favor elija un item del Menu o finalice su compra'
-      );
-      break;
-  }
-} while (selector != 0 && selector != 9);
-
-///////////////////////////////////////////////
 ////////////FUNCIONES MANIPULACIÓN DEL DOM////
 /////////////////////////////////////////////
 
-/////////////////Selección de nodos HTML/////////////////////////////////
+/////////////////Selección de elementos/////////////////////////////////
 
 const productContainer = document.querySelector('.productContainer');
+const formContainer = document.querySelector('.formContainer');
+const montoTotal = document.querySelector('.monto');
+const btnComprar = document.querySelector('.btn_submit');
+const btnReset = document.querySelector('.btn_reset');
 
-////////////////Inserción HTML Catalogo///////////////////////////
+////////////////Inserción dinámica HTML del menu de productos desde el catalogo///////////////
+
+//creamos un id dinámico para el input de cada producto para saber luego cual estoy modificando cuando utilice el eventlistener
 
 for (const producto of catalogo) {
   const indivProduct = document.createElement('div');
@@ -188,3 +114,49 @@ for (const producto of catalogo) {
             />`;
   productContainer.appendChild(indivProduct);
 }
+
+/////////////// Función - Inserción dinámica HTML de Monto Total a Pagar por cliente////////////
+
+function insertarTotal() {
+  montoTotal.innerHTML = `Total a Pagar: $${totalCarrito()}`;
+}
+
+/////////////// Capturar [id] y [cantidad] del input del Menu de productos al modificarlo////////////
+
+//listener en contenedor del form para mediante burbujeo capturar en que input estoy realizando modificaciones. Con e.target identifico [input] que se modifico y tomo valores
+
+formContainer.addEventListener('input', function (e) {
+  //si el campo del input está vacío lo hago valer 0
+  if (!e.target.value) {
+    e.target.value == 0;
+  }
+  //Actualizo carrito según valores del input que modifique con id (sku) y cantidad.
+  actualizarCarrito(parseInt(e.target.id), parseInt(e.target.value));
+  //Llamo función para insertar valor total a pagar en HTML
+  insertarTotal();
+});
+
+////////////////Evento -  Finalización de Compra///////////////////////////
+
+btnComprar.addEventListener('click', function (e) {
+  e.preventDefault();
+  // revisamos si hay items en el carrito y cerramos la compra. Si se encuentra vacio devolvemos un error al usuario y volvemos al menu principal
+  if (carrito.length > 0) {
+    //Finalización de compra efectiva. Se muestra recibo final con productos y monto total.
+    alert(`Gracias por su compra!
+  Su total es de $${totalCarrito()}
+  Productos:
+  ${mostrarCatalogo().join('\n')}`);
+  } else {
+    alert(
+      'Su carrito está vacio. Por favor agregue items para finalizar su compra.'
+    );
+  }
+});
+
+////////////////Evento - Borrado de Carrito y Formulario///////////////////////////
+
+btnReset.addEventListener('click', function () {
+  borrarCarrito();
+  insertarTotal();
+});
